f705a196f3336a06ee1676b8557f3a2f199af90f
Ayokunnumi1 <omololuayk@gmail.com>
2024-01-02 15:56:38 +0100

    Use helper method in application controller for forms

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+first_user = User.create(name: 'Joy')
+first_user_food = first_user.foods.create(name: 'Amala & Gbegiri soup', measurement_unit: 'grams', price: 100.00, quantity: 50)
+first_user_recipe = first_user.recipes.create(name: 'Amala & Gbegiri recipe', preparation_time: 10, cooking_time: 30, description: 'Boil water to prepare Amala, cook beans to prepare gbegiri', is_public: true )
