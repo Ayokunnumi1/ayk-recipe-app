5ca254b8d52bedde9c225b9858f871bb5380f354
Ayokunnumi1 <omololuayk@gmail.com>
2024-01-05 16:09:37 +0100

    Edit foods, fix the errors & display on UI

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -8,13 +8,21 @@ class FoodsController < ApplicationController
   end
 
   def create
-    @food_form_data = Food.new([:food_form_params])
-    if @food_form_data.save
+    @food_form = current_user.foods.build(food_form_params)
+    if @food_form.save
       flash[:success] = 'Food saved successfully'
       redirect_to foods_path
     else
       flash[:error] = 'Food Not Saved, try again later'
+      puts @food_form.errors.full_messages
       render :new
     end
   end
+
+  private
+
+  def food_form_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   root "foods#index"
   resources :users, only: [:index]
-  resources :foods, only: [:index, :show, :new]
-  resources :recipes, only: [:index, :show, :new]
+  resources :foods, only: [:index, :show, :new, :create]
+  resources :recipes, only: [:index, :show, :new, :create]
 end
