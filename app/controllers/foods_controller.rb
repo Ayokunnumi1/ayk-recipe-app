bd1c1c91c763f1d989f622a4cb6303b454334023
Ayokunnumi1 <omololuayk@gmail.com>
2024-01-05 11:57:15 +0100

    Create foods controller, views, & routes

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,8 +1,20 @@
 class FoodsController < ApplicationController
+  def index
+    @foods = Food.all
+  end
+
   def new
     @food_form = Food.new
   end
 
   def create
+    @food_form_data = Food.new([:food_form_params])
+    if @food_form_data.save
+      flash[:success] = 'Food saved successfully'
+      redirect_to foods_path
+    else
+      flash[:error] = 'Food Not Saved, try again later'
+      render :new
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,9 @@
 Rails.application.routes.draw do
-  resources :foods, only: [:index, :show, :new]
-  get 'recipes/new'
-  get 'recipes/create'
-  get 'foods/new'
-  get 'foods/create'
-  get 'users/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+ 
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root "foods#index"
+  resources :users, only: [:index]
+  resources :foods, only: [:index, :show, :new]
+  resources :recipes, only: [:index, :show, :new]
 end
