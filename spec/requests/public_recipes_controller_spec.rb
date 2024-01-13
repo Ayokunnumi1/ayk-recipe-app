d0ed474f36d6e82fc65e382ac08b339352b8c6b9
Ayokunnumi1 <omololuayk@gmail.com>
2024-01-13 08:37:07 +0100

    Include test for public recipe & recipe food controllers

--- spec/requests/public_recipes_controller_spec.rb
+++ spec/requests/public_recipes_controller_spec.rb
@@ -1,7 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe "PublicRecipesControllers", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe PublicRecipesController, type: :controller do
+  include Devise::Test::ControllerHelpers
+
+  let(:user) { User.create!(name: 'Test User', email: 'test@example.com', password: 'password') }
+  let(:public_recipe) { Recipe.create!(name: 'Test Recipe', description: 'Test Description', is_public: true, user: user) }
+  let(:private_recipe) { Recipe.create!(name: 'Private Recipe', description: 'Private Description', is_public: false, user: user) }
+
+  describe 'GET #index' do
+    it 'assigns only public recipes as @public_recipes' do
+      public_recipe
+      private_recipe
+      get :index
+      expect(assigns(:public_recipes)).to eq([public_recipe])
+    end
+
+    it 'assigns all users as @users' do
+      get :index
+      expect(assigns(:users)).to eq([user])
+    end
   end
 end
--- spec/requests/recipe_foods_controller_spec.rb
+++ spec/requests/recipe_foods_controller_spec.rb
@@ -1,7 +1,48 @@
 require 'rails_helper'
 
-RSpec.describe "RecipeFoodsControllers", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe RecipeFoodsController, type: :controller do
+  include Devise::Test::ControllerHelpers
+
+  let(:user) { User.create!(name: 'Test User', email: 'test@example.com', password: 'password') }
+  let(:recipe) { Recipe.create!(name: 'Test Recipe', description: 'Test Description', user: user) }
+  let(:food) { Food.create!(name: 'Test Food', user: user) }
+  let(:recipe_food) { RecipeFood.create!(recipe: recipe, food: food) }
+
+  before do
+    sign_in user
+  end
+
+  describe 'GET #new' do
+    it 'assigns a new recipe_food as @recipe_food' do
+      get :new, params: { recipe_id: recipe.id }
+      expect(assigns(:recipe_food)).to be_a_new(RecipeFood)
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'creates a new recipe_food' do
+        expect {
+          post :create, params: { recipe_food: { recipe_id: recipe.id, food_id: food.id } }
+        }.to change(RecipeFood, :count).by(1)
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the new recipe_food' do
+        expect {
+          post :create, params: { recipe_food: { recipe_id: nil, food_id: nil } }
+        }.to_not change(RecipeFood, :count)
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    it 'deletes the recipe_food' do
+      recipe_food
+      expect {
+        delete :destroy, params: { id: recipe_food.id }
+      }.to change(RecipeFood, :count).by(-1)
+    end
   end
 end
